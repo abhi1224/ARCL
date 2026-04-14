4b2c8972c17bd062f884235cf079620ae397e57c
Abhishek Verma <abhishek27501@gmail.com>
2026-04-14 07:19:35 +0530

    refactor: add all APIs for category

--- frontend/src/api/categoryApi.js
+++ frontend/src/api/categoryApi.js
@@ -1,4 +1,24 @@
 import API from "./axios";
 
+// GET
 export const getCategories = () => API.get("/categories");
-export const deleteCategory = (id) => API.delete(`/categories/${id}`);
+
+// CREATE
+export const createCategory = (data) =>
+  API.post("/categories", data);
+
+// UPDATE
+export const updateCategory = (id, data) =>
+  API.put(`/categories/${id}`, data);
+
+// DELETE (soft)
+export const deleteCategory = (id) =>
+  API.delete(`/categories/${id}`);
+
+// TOGGLE ACTIVE
+export const toggleCategoryActive = (id) =>
+  API.patch(`/categories/${id}/toggle-active`);
+
+// TOGGLE FEATURED
+export const toggleCategoryFeatured = (id) =>
+  API.patch(`/categories/${id}/toggle-featured`);
