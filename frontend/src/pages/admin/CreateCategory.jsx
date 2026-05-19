4c4a85f93f04852805831aa30b6eda12fd56edf4
Abhishek Verma <abhishek27501@gmail.com>
2026-05-19 12:39:38 +0530

    feat: create create category page

---
+++ frontend/src/pages/admin/CreateCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryForm from '../../components/admin/category/CategoryForm'
+
+const CreateCategory = () => {
+  return (
+    <>
+        <CategoryForm  mode='create' />
+    </>
+  )
+}
+
+export default CreateCategory
